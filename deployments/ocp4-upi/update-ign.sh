f0ce5de0e74b0009d9f3695b67ae3ff1efaf7bf0
Robert Bohne <robert.bohne@redhat.com>
2019-06-06 16:55:22 +0200

    Add DEST_DIR to update-ign.sh

--- deployments/ocp4-upi/update-ign.sh
+++ deployments/ocp4-upi/update-ign.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+DEST_DIR=/var/www/html/ocp4/
+cat bootstrap.ign | jq ".storage.files |= . + $(./static-ip.sh bootstrap.ocp4-upi.bohne.io 10.84.3.2)"  -c > ${DEST_DIR}bootstrap-0.ign
 
-cat bootstrap.ign | jq ".storage.files |= . + $(./static-ip.sh bootstrap.ocp4-upi.bohne.io 10.84.3.2)"  -c > bootstrap-0.ign
+cat master.ign | jq ".storage.files |= . + $(./static-ip.sh controller-0.ocp4-upi.bohne.io 10.84.3.30)"  -c > ${DEST_DIR}master-0.ign
+cat master.ign | jq ".storage.files |= . + $(./static-ip.sh controller-1.ocp4-upi.bohne.io 10.84.3.31)"  -c > ${DEST_DIR}master-1.ign
+cat master.ign | jq ".storage.files |= . + $(./static-ip.sh controller-2.ocp4-upi.bohne.io 10.84.3.32)"  -c > ${DEST_DIR}master-2.ign
 
-cat master.ign | jq ".storage.files |= . + $(./static-ip.sh controller-0.ocp4-upi.bohne.io 10.84.3.30)"  -c > master-0.ign
-cat master.ign | jq ".storage.files |= . + $(./static-ip.sh controller-1.ocp4-upi.bohne.io 10.84.3.31)"  -c > master-1.ign
-cat master.ign | jq ".storage.files |= . + $(./static-ip.sh controller-2.ocp4-upi.bohne.io 10.84.3.32)"  -c > master-2.ign
-
-cat worker.ign | jq ".storage.files |= . + $(./static-ip.sh worker-0.ocp4-upi.bohne.io 10.84.3.40)"  -c > worker-0.ign
-cat worker.ign | jq ".storage.files |= . + $(./static-ip.sh worker-1.ocp4-upi.bohne.io 10.84.3.41)"  -c > worker-1.ign
-cat worker.ign | jq ".storage.files |= . + $(./static-ip.sh worker-2.ocp4-upi.bohne.io 10.84.3.42)"  -c > worker-2.ign
+cat worker.ign | jq ".storage.files |= . + $(./static-ip.sh worker-0.ocp4-upi.bohne.io 10.84.3.40)"  -c > ${DEST_DIR}worker-0.ign
+cat worker.ign | jq ".storage.files |= . + $(./static-ip.sh worker-1.ocp4-upi.bohne.io 10.84.3.41)"  -c > ${DEST_DIR}worker-1.ign
+cat worker.ign | jq ".storage.files |= . + $(./static-ip.sh worker-2.ocp4-upi.bohne.io 10.84.3.42)"  -c > ${DEST_DIR}worker-2.ign
