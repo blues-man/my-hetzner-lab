625667e53fd636941482f762b5c3deb78d4311f5
Robert Bohne <robert.bohne@redhat.com>
2019-07-02 17:02:17 +0200

    Change offline solution, libvirt do not support dns resolving without nat..

--- deployments/ocp3-disconneted/scripts/install_openshift.sh
+++ deployments/ocp3-disconneted/scripts/install_openshift.sh
@@ -3,5 +3,6 @@ yum -y install openshift-ansible screen bind-utils
 sed -i "s/#host_key_checking/host_key_checking = True/" /etc/ansible/ansible.cfg
 sed -i "s/#log_path/log_path/" /etc/ansible/ansible.cfg
 
-cd /usr/share/ansible/openshift-ansible/
-ansible-playbook -i /root/inventory /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml && ansible-playbook -i /root/inventory /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+# Install by hand...
+#cd /usr/share/ansible/openshift-ansible/
+#ansible-playbook -i /root/inventory /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml && ansible-playbook -i /root/inventory /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
